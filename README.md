4d0fa522c75493ee58c10882c34a146b7d6a13fc
Dimitrios Stathis <stathis@kth.se>
2019-02-19 17:38:37 +0100

    License added

--- README.md
+++ README.md
@@ -63,3 +63,19 @@ To run the script run the following command:
 - python-docx
 
 To install in windows use pip3.exe install <package name> from a command window. For mac or linux you can use the pip3 command from the terminal.
+
+
+# LICENSE
+
+DocParser is free software: you can redistribute it and/or modify   
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or   
+(at your option) any later version.                                 
+                                                                    
+DocParser is distributed in the hope that it will be useful,        
+but WITHOUT ANY WARRANTY; without even the implied warranty of      
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the       
+GNU General Public License for more details.                 
+                                                                    
+You should have received a copy of the GNU General Public License   
+along with DocParser, see `COPYING` file.  If not, see <https://www.gnu.org/licenses/>. 
--- src/Doc_parse_v2.py
+++ src/Doc_parse_v2.py
@@ -4,7 +4,24 @@
 # email         : stathis@kth
 # Last edited   : 5/5/2018
 
-
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#                                                                         #
+#This file is part of DocParser.                                          #
+#                                                                         #
+#    DocParser is free software: you can redistribute it and/or modify    #
+#    it under the terms of the GNU General Public License as published by #
+#    the Free Software Foundation, either version 3 of the License, or    #
+#    (at your option) any later version.                                  #
+#                                                                         #
+#    DocParser is distributed in the hope that it will be useful,         #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of       #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        #
+#    GNU General Public License for more details.                         #
+#                                                                         #
+#    You should have received a copy of the GNU General Public License    #
+#    along with DocParser.  If not, see <https://www.gnu.org/licenses/>.  #
+#                                                                         #
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
 #This is a script for applying latex like references into a
 #microsoft word document (.docx)
